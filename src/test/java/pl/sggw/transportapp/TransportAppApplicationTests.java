32e2197d5195b3c4071726f03b9b0507ffd650a3
Marcin Bratek <marcin.bratek@akmf.pl>
2021-11-15 17:28:05 +0100

    initial-commit

---
+++ src/test/java/pl/sggw/transportapp/TransportAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sggw.transportapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransportAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
